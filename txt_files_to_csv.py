6dc4866e3c93d0e56a8973f82d058e15050cf35f
AnthBrun <149007192+AnthBrun@users.noreply.github.com>
2023-11-28 01:31:01 +0100

    convert_txt_files_to_csv.py

--- txt_files_to_csv.py
+++ txt_files_to_csv.py
@@ -1,10 +1,16 @@
-import sys, os, glob, subprocess
+import sys, os, glob, subprocess, time
 
 if len(sys.argv) == 2:
+    time.sleep(1)
+    print(f"start converting txt to csv")
     directory_path = os.path.join(os.getcwd(), sys.argv[1])
+    directory_path = os.path.join(directory_path)
+    print(f"directory_path: {directory_path}")
     pattern = '*.txt'
     file_path = glob.glob(os.path.join(directory_path, pattern))
+    print(f"filepath: {file_path}")
     files = [os.path.basename(file) for file in file_path]
+    print(f"files : {files}")
     for file in files:
         path = os.path.join(directory_path, file)
         cmd = f"python3 replace_semicolon_with_comma.py {os.path.join(sys.argv[1], file)}"
