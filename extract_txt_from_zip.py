f01e18d67cf2dee44fe32da5f0718d0169d56b54
AnthBrun <149007192+AnthBrun@users.noreply.github.com>
2023-11-28 01:31:36 +0100

    extract_txt_from_zip.py

--- extract_txt_from_zip.py
+++ extract_txt_from_zip.py
@@ -8,14 +8,14 @@ if len(sys.argv) == 2:
     if not os.path.exists(txt_path):
         target_path = os.makedirs(txt_path)
 
-    print(txt_path)
     matching_zip_path = glob.glob(os.path.join(directory_path, '*.zip'))
     for match in matching_zip_path:
         with zipfile.ZipFile(match, 'r') as zip_ref:
             file = [file for file in zip_ref.namelist() if re.match(r'produkt_[\w]*.txt', file)]
             zip_ref.extract(file[0], txt_path)
 
-    cmd = f"python3 txt_files_to_csv {txt_path}"
+    txt_path = txt_path.split('\\',-2)
+    cmd = f"python3 convert_txt_files_to_csv.py {txt_path[-2]}\\{txt_path[-1]}"
     process = subprocess.Popen(cmd, shell=True)
     process.communicate()
 else:
